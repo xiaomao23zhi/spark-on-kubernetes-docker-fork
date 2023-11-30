4599f83936ac388cdfbcb3ed120ff9c9ba8ad100
xiaomao23zhi <gokasama@icloud.com>
2023-11-30 10:43:46 +0800

    add build-spark

--- spark/3.1.1-hadoop-3.2/build.sh
+++ spark/3.1.1-hadoop-3.2/build.sh
@@ -7,7 +7,7 @@ HADOOP_VERSION=hadoop3.2
 AWS_JAVA_SDK_BUNDLE_VERSION=1.11.1034
 HADOOP_AWS_VERSION=3.2.4
 
-REPO=ghcr.io/meganote
+REPO=ghcr.io/xiaomao23zhi
 TAG=v${SPARK_VERSION}-${HADOOP_VERSION}
 
 wget -q https://archive.apache.org/dist/spark/spark-${SPARK_VERSION}/spark-${SPARK_VERSION}-bin-${HADOOP_VERSION}.tgz
